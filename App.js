ec8e69fc66164c14de92bee8470149027185f23d
ezeanic <ezeanic@uindy.edu>
2020-12-19 16:14:59 -0500

    App working and errors resolved.. user can play with computer

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ export default class App extends React.Component {
     super();
     this.state={
       countval: "X",
+      computerstate: "Your turn",
+      winGame: "",
       timearound: 0,
       scorecountuser: 0,
       scorecountcomputer: 0,
@@ -27,121 +29,267 @@ export default class App extends React.Component {
         ]});
       return;
     }
-    
+    this.state.winGame = ""
     let boxeslist = this.state.boxes;       
     
     if(this.state.boxes[val].value == ""){
-      // if(this.state.countval == "X"){
-      //   this.setState({countval: "O"});
-      // }else{
-      //   this.setState({countval: "X"});
-      // }
+      
       this.setState({countval: valtext});
-      // if(valtext == "O"){
-      //   do{
-      //       val = Math.floor(Math.random() * (9 - 0) + 0);
-      //     }while(boxeslist[val].clickable == false);
-      // }
+      
       boxeslist[val].value = valtext;
       boxeslist[val].clickable = false;
     }
     
     if(boxeslist[0].value=="X" && boxeslist[1].value=="X"  && boxeslist[2].value=="X" || boxeslist[0].value=="O" && boxeslist[1].value=="O" && boxeslist[2].value=="O"){
-      boxeslist[0].boxcolor = "green";
-      boxeslist[1].boxcolor = "green";
-      boxeslist[2].boxcolor = "green";
-
-      if(valtext == "X"){
-        let vald = this.state.scorecountuser++;
-        this.setState({scorecountuser: vald})
+      
+      if(valtext === "X"){
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[0].boxcolor = "green";
+        boxeslist[1].boxcolor = "green";
+        boxeslist[2].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[0].boxcolor = "red";
+        boxeslist[1].boxcolor = "red";
+        boxeslist[2].boxcolor = "red";
       }
+      this.state.computerstate="";
+
+      setTimeout(
+        function() {
+          this.setState({computerstate: "",
+          winGame: "", boxes: [{id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
+      
     }
     else if((boxeslist[3].value=="X" && boxeslist[4].value=="X"  && boxeslist[5].value=="X") || (boxeslist[3].value=="O" && boxeslist[4].value=="O" && boxeslist[5].value=="O")){
-      boxeslist[3].boxcolor = "green";
-      boxeslist[4].boxcolor = "green";
-      boxeslist[5].boxcolor = "green";
+      
 
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[3].boxcolor = "green";
+        boxeslist[4].boxcolor = "green";
+        boxeslist[5].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.computerstate++;
+        this.state.winGame = "Computer wins";
+        boxeslist[3].boxcolor = "red";
+        boxeslist[4].boxcolor = "red";
+        boxeslist[5].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({computerstate: "",
+          winGame: "",boxes: [{id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
+  
     }
     else if((boxeslist[6].value=="X" && boxeslist[7].value=="X"  && boxeslist[8].value=="X") || (boxeslist[6].value=="O" && boxeslist[7].value=="O" && boxeslist[8].value=="O")){
-      boxeslist[6].boxcolor = "green";
-      boxeslist[7].boxcolor = "green";
-      boxeslist[8].boxcolor = "green";
 
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[6].boxcolor = "green";
+        boxeslist[7].boxcolor = "green";
+        boxeslist[8].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[6].boxcolor = "red";
+        boxeslist[7].boxcolor = "red";
+        boxeslist[8].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({boxes: [{computerstate: "",
+          winGame: "",id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
     }
     else if((boxeslist[0].value=="X" && boxeslist[3].value=="X"  && boxeslist[6].value=="X") || (boxeslist[0].value=="O" && boxeslist[3].value=="O" && boxeslist[6].value=="O")){
-      boxeslist[0].boxcolor = "green";
-      boxeslist[3].boxcolor = "green";
-      boxeslist[6].boxcolor = "green";
 
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[0].boxcolor = "green";
+        boxeslist[3].boxcolor = "green";
+        boxeslist[6].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[0].boxcolor = "red";
+        boxeslist[3].boxcolor = "red";
+        boxeslist[6].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({boxes: [{computerstate: "",
+          winGame: "",id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
     }
     else if((boxeslist[1].value=="X" && boxeslist[4].value=="X"  && boxeslist[7].value=="X") || (boxeslist[1].value=="O" && boxeslist[4].value=="O" && boxeslist[7].value=="O")){
-      boxeslist[1].boxcolor = "green";
-      boxeslist[4].boxcolor = "green";
-      boxeslist[7].boxcolor = "green";
 
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[1].boxcolor = "green";
+        boxeslist[4].boxcolor = "green";
+        boxeslist[7].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[1].boxcolor = "red";
+        boxeslist[4].boxcolor = "red";
+        boxeslist[7].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({boxes: [{computerstate: "",
+          winGame: "",id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
     }
     else if((boxeslist[2].value=="X" && boxeslist[5].value=="X"  && boxeslist[8].value=="X") || (boxeslist[2].value=="O" && boxeslist[5].value=="O" && boxeslist[8].value=="O")){
-      boxeslist[2].boxcolor = "green";
-      boxeslist[5].boxcolor = "green";
-      boxeslist[8].boxcolor = "green";
 
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[2].boxcolor = "green";
+        boxeslist[5].boxcolor = "green";
+        boxeslist[8].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[2].boxcolor = "red";
+        boxeslist[5].boxcolor = "red";
+        boxeslist[8].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({boxes: [{computerstate: "",
+          winGame: "",id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
     }
     else if((boxeslist[0].value=="X" && boxeslist[4].value=="X"  && boxeslist[8].value=="X") || (boxeslist[0].value=="O" && boxeslist[4].value=="O" && boxeslist[8].value=="O")){
-      boxeslist[0].boxcolor = "green";
-      boxeslist[4].boxcolor = "green";
-      boxeslist[8].boxcolor = "green";
-
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[0].boxcolor = "green";
+        boxeslist[4].boxcolor = "green";
+        boxeslist[8].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[0].boxcolor = "red";
+        boxeslist[4].boxcolor = "red";
+        boxeslist[8].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({computerstate: "",
+          winGame: "",boxes: [{id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
     }
     else if((boxeslist[2].value=="X" && boxeslist[4].value=="X"  && boxeslist[6].value=="X") || (boxeslist[2].value=="O" && boxeslist[4].value=="O" && boxeslist[6].value=="O")){
-      boxeslist[2].boxcolor = "green";
-      boxeslist[4].boxcolor = "green";
-      boxeslist[6].boxcolor = "green";
 
       if(valtext == "X"){
-        this.setState({scorecountuser: this.state.scorecountuser++})
+        this.state.scorecountuser++;
+        this.state.winGame = "You win";
+        boxeslist[2].boxcolor = "green";
+        boxeslist[4].boxcolor = "green";
+        boxeslist[6].boxcolor = "green";
       }else{
-        this.setState({scorecountcomputer: this.state.scorecountcomputer++})
+        this.state.scorecountcomputer++;
+        this.state.winGame = "Computer wins";
+        boxeslist[2].boxcolor = "red";
+        boxeslist[4].boxcolor = "red";
+        boxeslist[6].boxcolor = "red";
       }
+      this.state.computerstate="";
+      setTimeout(
+        function() {
+          this.setState({boxes: [{computerstate: "",
+          winGame: "",id:0, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:1, value: "", boxcolor: "#d9dfe0", clickable: true},{id:2, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:3, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:4, value: "", boxcolor: "#d9dfe0", clickable: true},{id:5, value: "", boxcolor: "#d9dfe0", clickable: true},
+          {id:6, value: "", boxcolor: "#d9dfe0", clickable: true}, {id:7, value: "", boxcolor: "#d9dfe0", clickable: true},{id:8, value: "", boxcolor: "#d9dfe0", clickable: true},
+          ]});
+          return;
+        }
+        .bind(this),
+        2000
+      );
     }
     
     console.log("You play   -> " + this.state.scorecountuser);
     console.log("computer play  -> " + this.state.scorecountcomputer);
 
+    let preventinfinityloop = 0;
+
     if(this.state.timearound == 0){
         do{
         val = Math.floor(Math.random() * (9 - 0) + 0);
+        preventinfinityloop++;
+        if(preventinfinityloop > 10){
+          this.state.computerstate=""
+          this.state.winGame = "Tie"
+          return;
+        }
       }while(boxeslist[val].clickable == false);
       
       setTimeout(
@@ -149,11 +297,13 @@ export default class App extends React.Component {
           this.changevalState(val, "O");
         }
         .bind(this),
-        1000
+        3000
       );
       this.state.timearound = 1;
+      this.state.computerstate = "Computer is Thinking..."
     }else{
       this.state.timearound = 0;
+      this.state.computerstate = "Your turn..."
     }
     
     this.setState({boxes: boxeslist}); 
@@ -162,8 +312,12 @@ export default class App extends React.Component {
   render(){  
     return (
       <View style={styles.container}>
+        <Text style={styles.firstinfo}>{this.state.computerstate}</Text>
         <ActualPuzzle stateChange={stateInput => this.setState({ boxes: stateInput})} arrayBoxes={this.state.boxes} changevalState={(val, valtext)=>{this.changevalState(val, valtext)}}/>
-        <Text>{this.state.scorecountuser} {this.state.scorecountcomputer}</Text>
+        <Text style={styles.info}>You Computer</Text>
+        <Text style={styles.info}> {this.state.scorecountuser} {this.state.scorecountcomputer}</Text>
+        <Text style={styles.info}>{this.state.winGame}</Text>
+        
       </View>
     );
   }
@@ -177,5 +331,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: "#a8a9a8"
   },
+  firstinfo: {
+    top: "-5%",
+    fontSize: 20,
+  },
+  info: {
+    fontSize: 20,
+  }
   
 });
